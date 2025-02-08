ad98ed2cd97fd0d97814b6db4696dbf1b377e93f
pinpinvideodownloader <taynaelwiragm@gmail.com>
2025-02-08 19:38:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ if __name__ == '__main__':
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Usar a porta do Render
-    app.run(host='0.0.0.0', port=port)  # Permitir acesso externo
+    #app.run(host='0.0.0.0', port=port)  # Permitir acesso externo
 
